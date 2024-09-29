be54a00c0ef6b937f8f559e7580ae9b89295c45a
ngphuc2808 <ngphuc.2808@gmail.com>
2024-09-29 22:59:20 +0700

    set up source v3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const name: string = 'Phúc Dev'
-console.log(name)
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
